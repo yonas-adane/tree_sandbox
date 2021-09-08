9f8651ee44207d84005a758512157d7041f9a452
y2ky2k <yonas.kidanemariam@jewsforjesus.org>
2021-09-07 19:34:11 -0700

    xml mapping + json path

--- json_to_tree/src/main/java/json_to_tree.java
+++ json_to_tree/src/main/java/json_to_tree.java
@@ -9,9 +9,9 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class json_to_tree {
 
@@ -21,8 +21,8 @@ public class json_to_tree {
 
         try {
 
-            json_to_tree j = new json_to_tree();
-            j.xmlToObject();
+           json_to_tree j = new json_to_tree();
+//            j.xmlToObject();
 
             InputStream sourceStream = json_to_tree.class.getClassLoader().getResourceAsStream("source.json");
             InputStream targetStream = json_to_tree.class.getClassLoader().getResourceAsStream("target.json");
@@ -31,32 +31,35 @@ public class json_to_tree {
 
             ObjectMapper objectMapper = new ObjectMapper();
 
-
             JsonNode sourceJsonNode = objectMapper.readValue(sourceStream, JsonNode.class);
             JsonNode targetJsonNode = objectMapper.readValue(targetStream, JsonNode.class);
 
             JsonNode mapJsonNode = objectMapper.readValue(mapStream, JsonNode.class);
 
+            j.listPath(sourceJsonNode);
+            j.listPath(targetJsonNode);
 
-            for (JsonNode node: mapJsonNode) {
+            for (JsonNode mapNode: mapJsonNode) {
 
-                String sourcePath = node.get("sourcePath").textValue();
-                String targetPath = node.get("targetPath").textValue();
+                ArrayNode sourcePath = (ArrayNode) mapNode.get("sourcePath");
+                String targetPath = mapNode.get("targetPath").textValue();
 
-                String transformFunction = node.get("transformFunction").textValue();
+                String transformFunction = mapNode.get("transformFunction").textValue();
 
-                JsonNode sourceValue = extractPathValue(sourceJsonNode, sourcePath);
-                setPathValue(targetJsonNode, targetPath,sourceValue);
+                HashMap<String, JsonNode> sourceValues = extractPathValue(sourceJsonNode, sourcePath);
+
+                setPathValue(targetJsonNode, targetPath, sourceValues);
 
                 String sds = "";
             }
-
 
             Object f = sourceJsonNode.get("batters").get("batter");
 
             String ss = "";
 
         } catch (Exception ex) {
+
+
             ex.printStackTrace();
         }
 
@@ -80,71 +83,136 @@ public class json_to_tree {
         }
     }
 
-    private static JsonNode extractPathValue(JsonNode sourceJsonNode, String sourcePath) {
+    private static HashMap<String, JsonNode> extractPathValue(JsonNode sourceJsonNode, ArrayNode sourcePath) {
 
-        JsonNode sourceValue=null;
+        HashMap<String, JsonNode> sourceValues =  new HashMap<>();
 
-        String[] pathList = sourcePath.split("/");
+        for (JsonNode path: sourcePath) {
 
-        Integer i = 0;
-        for(String parent : pathList){
+            JsonNode sourceValue=null;
 
-            String xx = parent;
+            String[] pathList = path.textValue().split("/");
 
-            //root
-            if(pathList.length == 1){
-                sourceValue = sourceJsonNode.get(parent);
-            }
-            else {
-                //build path
-                if (i == 0) {
+            Integer i = 0;
+            for(String parent : pathList){
+
+                String xx = parent;
+
+                //root
+                if(pathList.length == 1){
                     sourceValue = sourceJsonNode.get(parent);
-                } else {
-                    sourceValue = sourceValue.get(parent);
                 }
-            }
+                else {
+                    //build path
+                    if (i == 0) {
+                        sourceValue = sourceJsonNode.get(parent);
+                    } else {
+                        sourceValue = sourceValue.get(parent);
+                    }
+                }
 
-            i++;
+                i++;
+
+                sourceValues.put(parent,sourceValue);
+
+            }
 
         }
 
-        return sourceValue;
+        return sourceValues;
     }
 
-    private static void setPathValue(JsonNode sourceJsonNode, String sourcePath, JsonNode value) {
+    private static void setPathValue(JsonNode sourceJsonNode, String sourcePath, HashMap<String, JsonNode> values) {
 
-        JsonNode sourceValue=null;
+        for (Map.Entry<String, JsonNode> entry : values.entrySet()) {
+            String key = entry.getKey();
+            JsonNode value = entry.getValue();
 
-        ObjectMapper mapper = new ObjectMapper();
+            JsonNode sourceValue = null;
 
-        String[] pathList = sourcePath.split("/");
+            ObjectMapper mapper = new ObjectMapper();
 
-        Integer i = 0;
-        for(String parent : pathList){
+            String[] pathList = sourcePath.split("/");
 
-            //root
-            if(pathList.length == 1) {
-                //((ObjectNode) sourceJsonNode.findParent(parent)).put(parent, value);
-                sourceValue = sourceJsonNode.get(parent);
-            }
-            else {
-                //build path
-                if (i == 0) {
+            Integer i = 0;
+            for (String parent : pathList) {
+
+                //root
+                if (pathList.length == 1) {
+                    //((ObjectNode) sourceJsonNode.findParent(parent)).put(parent, value);
                     sourceValue = sourceJsonNode.get(parent);
                 } else {
-                    sourceValue = sourceValue.get(parent);
+                    //build path
+                    if (i == 0) {
+                        sourceValue = sourceJsonNode.get(parent);
+                    } else {
+                        sourceValue = sourceValue.get(parent);
+                    }
+
+
                 }
 
+                i++;
 
             }
 
-            i++;
+            ((ObjectNode) sourceJsonNode.findParent(pathList[pathList.length - 1])).put(pathList[pathList.length - 1], value);
 
         }
 
-        ((ObjectNode) sourceJsonNode.findParent(pathList[pathList.length-1])).put(pathList[pathList.length-1], value);
+    }
+
+
+    private static void listPath(JsonNode json) {
+        getNextObject("", json);
+    }
+
+    private static void getNextObject(String parent, JsonNode data) {
+        if (data.isObject()) {
+            jsonObject(parent, (ObjectNode)data);
+        } else if (data.isArray()) {
+            jsonArray(parent, (ArrayNode) data);
+        } else {
+            jsonPrimitive(parent, data);
+        }
+    }
+
+    private static void jsonObject(String parent, JsonNode json) {
+
+
+        Iterator it = json.fields();
+
+        while (it.hasNext()) {
+            String key = ((Map.Entry<String, JsonNode>) it.next()).getKey();
+            JsonNode child = json.get(key);
+            String childKey = parent.isEmpty() ? key : parent + "/" + key;
+            getNextObject(childKey, child);
+        }
 
     }
+
+    private static void jsonArray(String parent, ArrayNode json) {
+
+        int i = 0;
+//        for (JsonNode node: json) {
+//
+//            JsonNode data = json.get(i);
+//            listObject(parent + "[" + i + "]", data);
+//
+//            i++;
+//        }
+
+        JsonNode data = json.get(i);
+        getNextObject(parent , data);
+
+
+    }
+
+    private static void jsonPrimitive(String parent, Object obj) {
+        //System.out.println(parent + ":"  + obj);
+        System.out.println(parent);
+    }
+
 
 
 
